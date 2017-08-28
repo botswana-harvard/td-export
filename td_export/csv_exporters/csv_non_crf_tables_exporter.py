75611a568d8bfc6c7c8283029b678af55ad9a437
erikvw <ew2789@gmail.com>
2017-08-28 13:52:01 +0200

    non crf

--- td_export/csv_exporters/csv_non_crf_tables_exporter.py
+++ td_export/csv_exporters/csv_non_crf_tables_exporter.py
@@ -1,24 +1,18 @@
-from edc_pdutils.csv_exporters import CsvCrfTablesExporter as Base
+from edc_pdutils.csv_exporters import CsvNonCrfTablesExporter
 from edc_pdutils.df_preppers import DfPrepper as DefaultPrepper
 
 from .td_csv_exporter import TdCsvExporter
 
 
-class CsvCrfTablesExporter(Base):
+class CsvNonCrfTablesExporter(CsvNonCrfTablesExporter):
     """
     Example Usage:
         $ ssh -f django@td.bhp.org.bw -L5001:localhost:3306 -N
 
         >>>
-        from edc_pdutils.mysqldb import Credentials
         from edc_pdutils.csv_exporters.tshilo_dikgotla import CsvCrfTablesExporter
-        credentials = Credentials(
-            user='<user>', passwd='<password>',
-            host='127.0.0.1', port='5001', dbname='bhp085')
-        tbl_exporter = CsvNonCrfTablesExporter(
-            app_label='td', credentials=credentials, exclude_history=True)
+        tbl_exporter = TdCsvNonCrfTablesExporter(app_label='td', exclude_history=True)
 
     """
     df_prepper_cls = DefaultPrepper
-    visit_column = 'maternal_visit_id'
     csv_exporter_cls = TdCsvExporter
