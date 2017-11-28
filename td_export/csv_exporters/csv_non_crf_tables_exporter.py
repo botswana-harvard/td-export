3a82b0daa79a7a5309ef431dae88c8097f845c74
erikvw <ew2789@gmail.com>
2017-11-28 18:27:37 +0200

    update to fix missing subject identifier in non-crf dataframes

--- td_export/csv_exporters/csv_non_crf_tables_exporter.py
+++ td_export/csv_exporters/csv_non_crf_tables_exporter.py
@@ -1,4 +1,6 @@
-from edc_pdutils import CsvTablesExporter
+import pandas as pd
+
+from edc_pdutils import CsvTablesExporter, TableToDataframe
 
 from ..df_handlers import TdNonCrfDfHandler
 from .td_csv_exporter import TdCsvExporter
@@ -18,3 +20,40 @@ class CsvNonCrfTablesExporter(CsvTablesExporter):
     app_label = 'td'
     df_handler_cls = TdNonCrfDfHandler
     csv_exporter_cls = TdCsvExporter
+
+    def __init__(self, **kwargs):
+        self._df_registered_subject = pd.DataFrame()
+        super().__init__(**kwargs)
+
+    @property
+    def df_registered_subject(self):
+        if self._df_registered_subject.empty:
+            columns = ['subject_identifier', 'relative_identifier', 'study_site',
+                       'dob', 'gender', 'screening_identifier', 'screening_datetime',
+                       'screening_age_in_years', 'registration_datetime', 'id']
+            obj = TableToDataframe(
+                table_name='edc_registration_registeredsubject',
+                columns=columns)
+            self._df_registered_subject = obj.dataframe.rename(
+                columns={'id': 'registered_subject_id'})
+        return self._df_registered_subject
+
+    def to_df(self, table_name=None, **kwargs):
+        """Returns a dataframe after passing the raw df
+        through the df_handler class.
+        """
+        df = self.get_raw_df(table_name)
+        rs_columns = list(self.df_registered_subject.columns)
+        df_columns = list(df)
+        columns = [
+            col for col in rs_columns if col not in df_columns] + df_columns
+        columns = [col for col in columns if not col.startswith('exported')]
+        df = pd.merge(df, self.df_registered_subject,
+                      on='registered_subject_id', how='left', suffixes=['', '_merged'])
+        df = df.loc[:, columns]
+        if self.df_handler_cls:
+            df_handler = self.df_handler_cls(
+                dataframe=df, db=self.db,
+                table_name=table_name, **kwargs)
+            df = df_handler.dataframe
+        return df
